40b2c0a41d741e7fee06d141b96452962a5fe1ba
Caiosouzadevv <caioss20.cs@gmail.com>
2025-11-12 21:38:00 -0300

    feat: including new header-component

--- espacopradodoscortes/src/app/app.component.ts
+++ espacopradodoscortes/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { BannerComponent } from './features/home/banner/banner.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
