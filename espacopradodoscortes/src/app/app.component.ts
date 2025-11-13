ab60817ce98e988d3ca6c7fe59fd26741d9f7892
Caiosouzadevv <caioss20.cs@gmail.com>
2025-11-12 22:03:44 -0300

    feat: new counter

--- espacopradodoscortes/src/app/app.component.ts
+++ espacopradodoscortes/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { BannerComponent } from './features/home/banner/banner.component';
+import { CounterComponent } from './features/home/components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BannerComponent],
+  imports: [RouterOutlet, HeaderComponent, BannerComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
