65ec160c52ec954124d792fa48392c90c5e8d743
Caiosouzadevv <caioss20.cs@gmail.com>
2025-11-14 11:10:09 -0300

    feat: new component - contact

---
+++ espacopradodoscortes/src/app/core/config/site-info.ts
@@ -0,0 +1,11 @@
+export const SITE_INFO = {
+    name: 'Espaço Prado dos Cortes',
+    address: 'Rua Abílio Primo Nalim, 157B',
+    district: 'Jardim Iracema',
+    city: 'São Paulo',
+    state: 'SP',
+    cep: '02805-200',
+    phone: '(11) 986248243',
+    whatsapp: '11986248243',
+    email: 'contato@pradodoscortes.com',
+};
