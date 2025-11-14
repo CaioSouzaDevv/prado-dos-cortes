633b1feea440367605bc6f9195388a25f49357be
Caiosouzadevv <caioss20.cs@gmail.com>
2025-11-13 23:22:00 -0300

    feat: ready leayout testimontials

--- espacopradodoscortes/src/app/app.component.ts
+++ espacopradodoscortes/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { BannerComponent } from './features/home/banner/banner.component';
 import { CounterComponent } from './features/home/components/counter/counter.component';
+import { TestimonialsComponent } from './features/home/components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BannerComponent, CounterComponent, ServicesComponent],
+  imports: [RouterOutlet, HeaderComponent, BannerComponent, CounterComponent, ServicesComponent, TestimonialsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
